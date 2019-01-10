071c6ee45c41726adcfaa6ce5f4eabc1dea88a37
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2019-01-10 11:56:26 +0530

    Create Segment Tree.cpp

---
+++ Segment Tree.cpp
@@ -0,0 +1,163 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+const int INF = int(2e9);
+
+#define PlUS_WRAP(index,n) index = (index+1)%n		//index++; if(index>=n) index=0
+#define MINUS_WRAP(index,n) index = (index + n -1)%n 	//index--; if(index<0) index=n-1
+
+const int N = 1e6 + 6;
+
+class Segment_Tree{
+
+private:
+	std::vector<int>segment;
+	std::vector<int> arr;
+	std::vector<int> lazy;
+	int n;
+
+
+	void push( int node , int l , int r  ){
+
+		if(lazy[node]){
+			segment[node] = lazy[node];
+			if( l != r ){
+				lazy[node+node] = lazy[node];
+				lazy[node+node +1] = lazy[node];
+			}
+			lazy[node] = 0;
+		}
+	}
+
+	void build( int node , int l , int r ){
+
+		if( l == r-1 ){
+			segment[node] = arr[l];
+			return;
+		}
+		int mid = ( l + r )>>1;
+		build( node + node , l , mid );
+		build( node+node+1 , mid +1 , r );
+
+
+		// Operation of segment Tree
+
+		segment[node]= max( segment[node+node] , segment[node+node+1]);
+	}
+
+	long long range_query( int node , int l , int r , int ql , int qr ){
+
+		if( l >=qr || r <=ql ) return -INF;
+
+		push( node , l , r );
+
+		if( ql <=l && r <= qr ) return segment[node];
+		int mid = ( l + r )>>1;
+		long long left_ans = range_query( node + node , l , mid , ql , qr );
+		long long right_ans = range_query( node + node +1 , mid +1 , r , ql , qr);
+
+		// Operation of segment tree
+
+		return max( left_ans , right_ans );
+
+	}
+
+
+	void point_update( int node , int l , int r , int idx , int val ){
+
+		if( r <= idx || l > idx  ) return ;
+
+		if( l == r-1 || l == idx ){
+			segment[node] = val;
+			arr[l] = val;
+			return ;
+		}
+
+		int mid = ( l + r )>>1;
+
+		point_update( node + node , l , mid , idx , val );
+		point_update( node + node +1 , mid +1 , r , idx , val );
+
+		// Operation of segment tree
+
+		segment[node] = max( segment[node + node ] , segment[node+node+1]);
+
+	}
+
+
+	void range_update( int node , int l , int r , int ql , int qr , int val ){
+
+		push( node , l , r );
+
+		if( l>= qr ||  r <= ql ) return ;
+
+		if( ql <=l && r <= qr ){
+
+			segment[node] = val;
+			if( l != r-1 ){
+				lazy[node + node ] = val;
+				lazy[node+ node+ 1 ] = val;
+			}
+			return ;
+		}
+
+		int mid = (  l + r )>>1;
+		range_update( node + node , l , mid , ql , qr , val );
+		range_update( node + node +1 , mid +1 , r , ql , qr , val );
+
+		// operation of segment tree
+
+		segment[node] = max( segment[node+node] , segment[node+node+1]);
+
+	}
+
+public:
+
+	Segment_Tree(){
+		segment.clear();
+		arr.clear();
+		lazy.clear();
+		n = 0;
+	}
+
+	// construct using a vector
+	Segment_Tree( std::vector<int> &baseArray ){
+
+		arr = baseArray;
+		n  = (int)(arr.size());
+		segment = std::vector<int>(4*n , 0 );
+		lazy = std::vector<int>(4*n , 0 );
+		build( 1 , 0 , n );
+	}
+
+
+	// construct using an array
+	Segment_Tree( int *begin , int *end ){
+
+		arr = std::vector<int>( begin , end );
+		n  = (int)(arr.size());
+		segment = std::vector<int> (4*n , 0 );
+		lazy = std::vector<int> (4*n , 0 );
+		build( 1 , 0 , n );
+	}
+
+	long long query( int l , int  r ){
+		return range_query( 1 , 0 , n , l , r);
+	}
+
+	void point_update( int idx , int val ){
+		point_update( 1 , 0 , n , idx , val );
+	}
+
+	void range_update( int l , int r , int val ){
+		range_update( 1 , 0 , n , l ,  r , val );
+	}
+
+
+
+};
+
+
+int main(){
+    
+}
